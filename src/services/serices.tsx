b3a4d8855fb4518ec784e617b79e27ae9b5e67af
Widzmarc Phelle <widzmarc@gmail.com>
2025-07-07 01:48:08 -0400

    Create login page

---
+++ src/services/serices.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+const client = axios.create({
+    baseURL: API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+export const getData = () => {
+    return client.get('/')
+    .then(res => res.data)
+    .catch(err => err)
+}
